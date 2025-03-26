91fbc98b987cbc73cf4313303637fbe50a5910e5
Unegbu Kingsley <kingsobino@gmail.com>
2025-03-26 16:10:47 +0100

    file expansion

--- lib/fileverse/cli.rb
+++ lib/fileverse/cli.rb
@@ -6,8 +6,11 @@ module Fileverse
   # CLI class
   class CLI < Thor
     desc "new file template", "generate a interface for file"
-    def new(file_path)
-      p Fileverse::Files.find file_path
+    def new(path)
+      full_path = Fileverse::Files.expanded_path(path)
+      full_hidden_path = Fileverse::Files.expanded_hidden_path(full_path)
+      Fileverse::Files.create_hidden_file(full_hidden_path)
+      # p Fileverse::Files.create_hidden_file file_path
     end
   end
 end
--- lib/fileverse/files.rb
+++ lib/fileverse/files.rb
@@ -2,9 +2,30 @@
 
 module Fileverse
   module Files # rubocop:disable Style/Documentation
-    def self.find(file_path)
-      path = File.expand_path file_path, Dir.pwd
+    module_function
+
+    def expanded_path(path)
+      path = File.expand_path path, Dir.pwd
       throw Fileverse::FileNotFoundError.new(file_path) unless File.exist? path
+      path
+    end
+
+    def expanded_hidden_path(path)
+      path = File.join(File.dirname(path), ".verse.#{File.basename(path)}")
+      File.expand_path path, Dir.pwd
+    end
+
+    def create_hidden_file(path)
+      File.open(path, "w") do |writer|
+        writer.write(inital_header)
+      end
+    end
+
+    def inital_header
+      <<~INITIAL_HEADER
+        ######0
+        ######>
+      INITIAL_HEADER
     end
   end
 end
