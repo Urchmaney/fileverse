6bf51d0a0899a30080cf9877807a29f728c0ec65
Unegbu Kingsley <kingsobino@gmail.com>
2025-03-27 15:50:06 +0100

    introduce parser logic

--- lib/fileverse/files.rb
+++ lib/fileverse/files.rb
@@ -4,28 +4,20 @@ module Fileverse
   module Files # rubocop:disable Style/Documentation
     module_function
 
-    def expanded_path(path)
+    def expand_path(path)
       path = File.expand_path path, Dir.pwd
       throw Fileverse::FileNotFoundError.new(file_path) unless File.exist? path
       path
     end
 
-    def expanded_hidden_path(path)
+    def expand_hidden_path(path)
       path = File.join(File.dirname(path), ".verse.#{File.basename(path)}")
       File.expand_path path, Dir.pwd
     end
 
-    def create_hidden_file(path)
-      File.open(path, "w") do |writer|
-        writer.write(inital_header)
-      end
-    end
-
-    def inital_header
-      <<~INITIAL_HEADER
-        ######0
-        ######>
-      INITIAL_HEADER
+    def read(path)
+      full_path = expand_path(path)
+      File.read(full_path)
     end
   end
 end
---
+++ lib/fileverse/parser.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Fileverse
+  module Parser
+    # Header class to process header section
+    class Header
+      CLOSE_TAG = "######>"
+      def self.inital_header
+        <<~INITIAL_HEADER
+          <######0
+          #{CLOSE_TAG}
+        INITIAL_HEADER
+      end
+
+      attr_reader :path, :cursor, :iterator
+
+      def initialize(path)
+        @path = path
+        @iterator = File.foreach(path, chomp: true)
+      end
+
+      def parse
+        verify_first_header
+        parse_header_lines
+      end
+
+      def verify_first_header
+        first_line = iterator.next
+        /\A<\#{6}(?<cursor>\d+)\z/ =~ first_line
+        throw CorruptFormat.new unless cursor
+        @cursor = cursor
+      end
+
+      def parse_header_lines
+        loop do
+          line = iterator.next
+          break if line == CLOSE_TAG
+        end
+      end
+    end
+  end
+end
