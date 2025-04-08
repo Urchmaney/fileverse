797a8d8fd1a07200ff04900d1225c2eac2167779
Kingsley uche <kingsobino@gmail.com>
2025-04-08 12:43:54 +0100

    remove unused code

--- lib/fileverse.rb
+++ lib/fileverse.rb
@@ -8,11 +8,6 @@ require_relative "fileverse/previewer"
 
 # Parent module
 module Fileverse
-  def self.create_hidden_file(path)
-    File.open(path, "w") do |writer|
-      writer.write(inital_header)
-    end
-  end
 end
 
 require_relative "fileverse/cli"
